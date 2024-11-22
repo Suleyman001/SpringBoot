172ba76b78ba58973883eb308d26e0de62225501
Suleyman Jumaniyazov <suleyman290401@gmail.com>
2024-11-22 23:56:25 +0100

    creating project

---
+++ src/main/java/spring/eclasssystem/EClassSystemApplication.java
@@ -0,0 +1,13 @@
+package spring.eclasssystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EClassSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EClassSystemApplication.class, args);
+    }
+
+}
